e59d0cf4eae30cccc7b1b5a619961337264a56d6
Pranay Anand <pranayanand123@gmail.com>
2018-07-17 12:30:32 +0530

    Add files via upload

--- segmentation.py
+++ segmentation.py
@@ -10,6 +10,11 @@ import numpy as np
 import imutils
 from imutils import contours
 from skimage import measure
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+
+model = load_model('ocr_model.h5')
+lb = ['0','1','2','3','4','5','6','7','8','9']
 
 real = cv2.imread('roi.jpg',0)
 real = imutils.resize(real, width=300)
@@ -20,7 +25,10 @@ image = cv2.imread('roi.jpg',0)
 image = imutils.resize(image, width=300)
 ret, thresh = cv2.threshold(image, 20, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 bordersize=10
+
 thresh = cv2.copyMakeBorder(thresh, top=bordersize, bottom=bordersize, left=bordersize, right=bordersize, borderType= cv2.BORDER_CONSTANT, value=[0,0,0] )
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+thresh  = cv2.erode(thresh, kernel, iterations = 1)
 labels = measure.label(thresh, neighbors=8, background=0)
 mask = np.zeros(thresh.shape, dtype="uint8")
 for label in np.unique(labels):
@@ -29,13 +37,14 @@ for label in np.unique(labels):
 	labelMask = np.zeros(thresh.shape, dtype="uint8")
 	labelMask[labels == label] = 255
 	numPixels = cv2.countNonZero(labelMask)
-	if numPixels > 100:
+	if numPixels > 60:
 		mask = cv2.add(mask, labelMask)
 		
 cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-#cnts = contours.sort_contours(cnts)[0]
+cnts = contours.sort_contours(cnts)[0]
+final_result = ""
 print(len(cnts))
 for (i, c) in enumerate(cnts):
 	(x,y,w,h) = cv2.boundingRect(c)
@@ -44,8 +53,15 @@ for (i, c) in enumerate(cnts):
 	roi = real[y:y+h,x:x+w]
 	bordersize=10
 	roi = cv2.copyMakeBorder(roi, top=bordersize, bottom=bordersize, left=bordersize, right=bordersize, borderType= cv2.BORDER_CONSTANT, value=[255,255,255] )
-	out = 'roi%d.jpg'%i
-	cv2.imwrite(out,roi)
+	roi=cv2.resize(roi,(37,50))
+	roi=roi.astype('float32')/255.0
+	roi=img_to_array(roi)
+	roi=np.expand_dims(roi,axis=0)
+	prob = model.predict(roi)[0]
+	index= np.argmax(prob)
+	label=lb[index]
+	final_result+=label
+print(final_result)
 cv2.imshow('real',real)
 cv2.imshow('image', image)
 cv2.imshow('thresh',thresh)
